3ba619960edf6aad8e0dbd9a1582eb7da6deabfb
Sridharan <sridharan1311b@gmail.com>
2025-07-16 23:00:46 +0530

    fakestore 3 API

--- Ecommerse/src/app/login/login.ts
+++ Ecommerse/src/app/login/login.ts
@@ -19,7 +19,7 @@ export class Login implements OnInit{
   ngOnInit(): void {
       this.formValues = this.fb.group({
         email:['mor_2314',Validators.required],
-        password:['83r5^_',[Validators.required,Validators.minLength(8)]]
+        password:['83r5^_',Validators.required]
       })
   }
 
